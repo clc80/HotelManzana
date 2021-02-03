2a32c77a46af4572eb2922c4c780ad7812e7cf27
Claudia Maciel <clc_80@yahoo.com>
2021-02-02 20:48:14 -0800

    Collect Dates

--- HotelManzana/Controller/AddRegistriationTableViewController.swift
+++ HotelManzana/Controller/AddRegistriationTableViewController.swift
@@ -13,20 +13,50 @@ class AddRegistriationTableViewController: UITableViewController {
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     
+    @IBOutlet var checkInDateLabel: UILabel!
+    @IBOutlet var checkInDatePicker: UIDatePicker!
+    @IBOutlet var checkOutDatePicker: UIDatePicker!
+    @IBOutlet var checkOutDateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+        updateDateViews()
 
     }
+    
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
         
         print("DONE Tapped")
         print("firstName: \(firstName)")
         print("lastName: \(lastName)")
         print("email: \(email)")
+        print("checkIn: \(checkInDate)")
+        print("checkOut: \(checkOutDate)")
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+
     }
     
 }
